96d69df461ea27d8d8e6e99b5170e72a25317801
Manjushree Berike Rajanna <manju_br@Manjushrees-MBP.attlocal.net>
2021-11-17 15:33:04 -0800

    Integrated frontend and backend

--- back/controller/flight.js
+++ back/controller/flight.js
@@ -3,11 +3,17 @@ import {flightsMapper} from '../index.js';
 
 
 export const getFlights = async (req, res) => {
+    let { carrier, dayofmonth, dayofweek, origin_city, destination_city} = req.query;
+     dayofmonth = dayofmonth ? dayofmonth: 19;
+     dayofweek = dayofweek ? dayofweek: 5;
+     carrier = carrier ? carrier: 'DL';
+     origin_city = origin_city ? origin_city:'Detroit';
+     destination_city = destination_city ? destination_city:'Miami';
     try{
-        const query = 'SELECT * FROM flightnewtable WHERE origin_state = ? ALLOW FILTERING';
-        flightsMapper.getFlights = flightsMapper.mapWithQuery(query,flight => [ flight.origin_state ]);
+        const query = 'SELECT * FROM flightnewtable WHERE carrier = ? and dayofmonth = ? and dayofweek = ? and origin_city = ? and destination_city = ? ALLOW FILTERING';
+        flightsMapper.getFlights = flightsMapper.mapWithQuery(query,flight => [ flight.carrier, flight.dayofmonth,flight.dayofweek,flight.origin_city,flight.destination_city]);
 
-        const flightsData = await flightsMapper.getFlights({origin_state: 'MO'});
+        const flightsData = await flightsMapper.getFlights({carrier,dayofmonth,dayofweek,origin_city,destination_city});
         
         res.status(200).json(flightsData);
     } catch(error){
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import {
   Row, Col, Image, ListGroup, Card, Button, Form, Modal, Jumbotron, Container,
 } from 'react-bootstrap';
 import background from './background.jpeg'; 
 
 
+
 function App() {
+  const [origin, setOrigin] = useState("");
+  const [destination, setDestination] = useState("");
+  const [DepartureDate, setDepartureDate] = useState("");
+  const [Carrier, setCarrier] = useState("");
+  const [flights, setFlights] = useState([]);
+
+  const getFlightData = (event) => {
+    event.preventDefault();
+    let week = DepartureDate.split('-')[1];
+    let month = DepartureDate.split('-')[2];
+    // alert(week + " " + month);
+    if(week[0] === '0'){
+      week = week.substring(1, week.length);
+    }
+    if(month[0] === '0'){
+      month = month.substring(1, month.length);
+    }
+    // alert(week + " " + month);
+    fetch(`http://localhost:5000/api/flights?carrier=${Carrier}&dayofmonth=${month}&dayofweek=${week}&origin_city=${origin}&destination_city=${destination}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setFlights(result._rs.rows);
+          console.log(result._rs.rows);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log(error);
+        }
+      )
+
+  }
+ 
 
   return (
     
@@ -18,10 +54,8 @@ function App() {
             </h1>
             <p style={{ color: 'white' }}>
               •
-              Find cheap flights
+              Plan your vacation with cheap flights
               •
-            </p>
-            <p>
             </p>
           </Jumbotron>
 
@@ -39,6 +73,7 @@ function App() {
               </ListGroup>
               <hr></hr>
               <input type="radio" value="oneway" name="genbookingtypeder" /> One way
+              {' '}
               <input type="radio" value="round" name="bookingtype" /> Round Trip
               <hr></hr>
           
@@ -49,18 +84,18 @@ function App() {
         <Row>
        <Col>
         <label className="text-muted">From:</label>
-        <input  type="text" className="form-control" placeholder="Leaving from" />
+        <input  type="text" onChange={(event)=>setOrigin(event.target.value)} className="form-control" placeholder="Leaving from" value={origin}/>
         </Col>
         <Col>
-        <label className="text-muted">Departure Date:</label>
-        <input  type="date" className="form-control"/>
+        <label className="text-muted" >Departure Date:</label>
+        <input  type="date" onChange={(event)=>setDepartureDate(event.target.value)}  className="form-control" value={DepartureDate}/>
         </Col>
 
         </Row><hr></hr>
         <Row>
        <Col>
         <label className="text-muted">To:</label>
-        <input  type="text" className="form-control" placeholder="Going to" />
+        <input  type="text" onChange={(event)=>setDestination(event.target.value)} className="form-control" placeholder="Going to" value={destination}/>
         </Col>
         <Col>
         <label className="text-muted">Arrival Date:</label>
@@ -71,9 +106,9 @@ function App() {
       </div>
       <div className="form-group">
         <label className="text-muted">Carrier</label>
-        <input  type="text" className="form-control"/>
+        <input  type="text" onChange={(event)=>setCarrier(event.target.value)} className="form-control" placeholder="Enter Carrier"value={Carrier}/>
     </div>
-        <Button type="submit" value="Search" variant="info">
+        <Button type="submit" value="Search" variant="info" onClick={(event)=>getFlightData(event)}>
           Serach
         </Button>
         
@@ -83,6 +118,48 @@ function App() {
             </Col>
       </Row>
       </Container>
+               <Container>
+               {flights && flights.map((flight, i) => (
+                 <Row>
+                   <Col sm={5} md={4} lg={12}>
+                 <Card className="my-3 p-3 rounded">
+
+           <Card.Body style={{ fontFamily: 'initial' }}>
+             <Card.Title as="div">
+
+               <strong>
+                 {' '}
+                 Carrier: {flight.carrier}
+                 {' '}
+               </strong>
+               {' '}
+
+             </Card.Title>
+             <Card.Text as="div">
+             <ListGroup>
+             Departure Date: {flight.dayofmonth}/{flight.dayofweek}/2021
+             </ListGroup>
+             <ListGroup>
+             Origin city: {flight.origin_city}
+             </ListGroup>
+             <ListGroup>
+             Destination city: {flight.destination_city}
+             </ListGroup>
+             
+             </Card.Text>
+           <hr></hr>
+             <Button type="submit" value="Search" variant="success">
+          Book Flight
+        </Button>
+           </Card.Body>
+         </Card>
+         </Col>
+                 </Row>
+             
+                   ))}
+                      
+
+                  </Container>
      
     
     
